f5c9352a37bc9a5b1fce2e6d1974310488d51df3
Grace Ko <gracesbot@gmail.com>
2020-12-01 19:28:20 -0800

    refactored state and added SeasonDisplay content

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { lat: null, errorMessage:''};
+
+  state = { lat: null, errMessage: '' };
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-       this.setState({ errorMessage: err.message }
-       )}
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
@@ -21,10 +18,12 @@ class App extends React.Component {
       return <div>Error: {this.state.errorMessage}</div>
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>
+      return <SeasonDisplay lat={this.state.lat} />
     }
     return <div>Loading...</div>;
   }
+
+
 }
 
 
